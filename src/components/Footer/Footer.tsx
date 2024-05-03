6a7e9e6fe8933bf6e5fa141d6363f74ca5e44c31
unknown <gwenpena21@gmail.com>
2024-05-03 09:51:01 +0800

    Added footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,14 @@
+import styles from "@/components/Footer/footer.module.css";
 
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+  return <div className={styles.container}>
+    <div className={styles.logo}>
+        Blog
+    </div>
+    <div className={styles.text}>
+      Blog &copy; All rights reserved.
+    </div>
+  </div>;
 }
 
 export default Footer
